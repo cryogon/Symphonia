57312b554193eb0224432cc6b72d0e6a39bca397
cryogon <jtkalga@gmail.com>
2024-12-29 15:36:51 +0530

    feat: pause command

---
+++ src/discord-commands/music/pause.ts
@@ -0,0 +1,21 @@
+import {
+  CommandInteraction,
+  SlashCommandBuilder,
+} from "discord.js";
+import { state } from "../../states";
+import { AudioPlayerStatus } from "@discordjs/voice";
+
+export const data = new SlashCommandBuilder()
+  .setName("pause")
+  .setDescription("Pause Music");
+
+export async function execute(interaction: CommandInteraction) {
+  if (!interaction.guildId) {
+    return await interaction.reply("something is wrong");
+  }
+  state.pauseMusic(interaction.guildId);
+  const player = state.getAudioPlayer(interaction.guildId);
+  const content =
+    player.state.status === AudioPlayerStatus.Paused ? "Playing" : "Paused";
+  await interaction.reply({ content, ephemeral: true });
+}
